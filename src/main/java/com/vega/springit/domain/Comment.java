caec4d43f677f839a8dd33f2592f38bf6ed9c023
mohamed-cisse <77202196+mohamed-cisse@users.noreply.github.com>
2022-03-22 18:39:12 +0200

    link and comment onetomany relashin

--- src/main/java/com/vega/springit/domain/Comment.java
+++ src/main/java/com/vega/springit/domain/Comment.java
@@ -3,9 +3,7 @@ package com.vega.springit.domain;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Data
@@ -15,8 +13,10 @@ public class Comment {
     @GeneratedValue
     private long id;
     private String body;
-
+    @ManyToOne
+    private Link link;
     //link
+
 
 
 }
--- src/main/java/com/vega/springit/domain/Link.java
+++ src/main/java/com/vega/springit/domain/Link.java
@@ -1,11 +1,15 @@
 package com.vega.springit.domain;
 
+import com.sun.istack.NotNull;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Data
@@ -14,10 +18,14 @@ public class Link {
     @Id
     @GeneratedValue
     private long id;
+    @NotNull
     private String title;
+    @NotNull
     private String url;
 
     // comment
+    @OneToMany(mappedBy = "link")
+    private List<Comment> comments=new ArrayList<>();
 
 
 }
