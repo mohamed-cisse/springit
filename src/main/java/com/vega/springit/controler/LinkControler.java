1736b1911f5a9f5baa8ca4b427944c0cf06d1e12
mohamed-cisse <77202196+mohamed-cisse@users.noreply.github.com>
2022-03-27 20:04:35 +0200

    thymleaf foo

--- src/main/java/com/vega/springit/controler/LinkControler.java
+++ src/main/java/com/vega/springit/controler/LinkControler.java
@@ -3,46 +3,56 @@ package com.vega.springit.controler;
 import com.vega.springit.domain.Link;
 import com.vega.springit.repository.LinkRepository;
 import org.springframework.jmx.export.annotation.ManagedResource;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import javax.persistence.ManyToOne;
 import java.util.List;
 import java.util.Optional;
-
-@RequestMapping("links")
+//@RestController
+//@RequestMapping("/links")
+@Controller
 public class LinkControler {
 
-    private LinkRepository linkRepository;
+//    private LinkRepository linkRepository;
+//
+//    public LinkControler(LinkRepository linkRepository1)
+//    {
+//        this.linkRepository=linkRepository1;
+//    }
+//
 
-    public LinkControler(LinkRepository linkRepository1)
-    {
-        this.linkRepository=linkRepository1;
+
+        @GetMapping("/foo")
+        public String foo(Model model)
+        {
+            model.addAttribute("pageTitle","this is foo");
+            return "foo";
+        }
+//
+//        @GetMapping("/")
+//        public List<Link> list() {
+//            return linkRepository.findAll();
+//        }
+//
+//        @PostMapping("/create")
+//        public Link create(@ModelAttribute Link link) {
+//            return linkRepository.save(link);
+//        }
+//        @GetMapping("/{id}")
+//        public Optional<Link> read(@PathVariable Long id) {
+//            return linkRepository.findById(id);
+//        }
+//        @PutMapping("/{id}")
+//        public Link update(@PathVariable Long id,@ModelAttribute Link link) {
+//            //get id
+//            return linkRepository.save(link);
+//        }
+//        @DeleteMapping("/{id}")
+//        public void delete(@PathVariable Long id) {
+//            linkRepository.deleteById(id);
+//
+//        }
     }
-    @RestController
-    public class LinkController {
 
-        @GetMapping("/")
-        public List<Link> list() {
-            return linkRepository.findAll();
-        }
-
-        @PostMapping("/create")
-        public Link create(@ModelAttribute Link link) {
-            return linkRepository.save(link);
-        }
-        @GetMapping("/{id}")
-        public Optional<Link> read(@PathVariable Long id) {
-            return linkRepository.findById(id);
-        }
-        @PutMapping("/{id}")
-        public Link update(@PathVariable Long id,@ModelAttribute Link link) {
-            //get id
-            return linkRepository.save(link);
-        }
-        @DeleteMapping("/{id}")
-        public void delete(@PathVariable Long id) {
-            linkRepository.deleteById(id);
-
-        }
-    }
-}
