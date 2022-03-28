51b46d134152d3e116ef1b3a3a4130201cb4a056
mohamed-cisse <77202196+mohamed-cisse@users.noreply.github.com>
2022-03-28 18:31:49 +0200

    link view

--- src/main/java/com/vega/springit/controler/LinkController.java
+++ src/main/java/com/vega/springit/controler/LinkController.java
@@ -2,18 +2,42 @@ package com.vega.springit.controler;
 
 import com.vega.springit.domain.Link;
 import com.vega.springit.repository.LinkRepository;
-import org.springframework.jmx.export.annotation.ManagedResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
-import javax.persistence.ManyToOne;
-import java.util.List;
 import java.util.Optional;
+
 //@RestController
 //@RequestMapping("/links")
 @Controller
-public class LinkControler {
+public class LinkController {
+
+    LinkRepository linkRepository;
+    public LinkController(LinkRepository linkRepository)
+    {
+         this.linkRepository=linkRepository;
+    }
+    @GetMapping("/")
+    public String list(Model model)
+    {
+        model.addAttribute("Links",linkRepository.findAll());
+        return "link/list";
+    }
+    @GetMapping("/link/{id}")
+    public String read(@PathVariable Long id, Model model) {
+
+
+        Optional<Link> link= linkRepository.findById(id);
+        if(link.isPresent())
+        {
+        model.addAttribute("link",link.get());
+            return "link/view";
+        }else
+            return "redirect:/";
+
+    }
 
 //    private LinkRepository linkRepository;
 //
@@ -24,12 +48,12 @@ public class LinkControler {
 //
 
 
-        @GetMapping("/foo")
-        public String foo(Model model)
-        {
-            model.addAttribute("pageTitle","this is foo");
-            return "foo";
-        }
+//        @GetMapping("/foo")
+//        public String foo(Model model)
+//        {
+//            model.addAttribute("pageTitle","this is foo");
+//            return "foo";
+//        }
 //
 //        @GetMapping("/")
 //        public List<Link> list() {
--- src/main/java/com/vega/springit/repository/LinkRepository.java
+++ src/main/java/com/vega/springit/repository/LinkRepository.java
@@ -2,12 +2,13 @@ package com.vega.springit.repository;
 
 import com.vega.springit.domain.Link;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import javax.naming.ldap.LdapReferralException;
 
 public interface LinkRepository extends JpaRepository<Link,Long> {
   //  Link findByTitle(String title);
 
- Link findByTitle(String titel);
+ Link findByTitle(String title);
 
 }
